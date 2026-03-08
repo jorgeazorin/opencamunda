7d6a48a74ceb62029862787f16b0eb94b4fceea3
Jorge <jorge.azorin@gmail.com>
2026-03-08 15:07:58 +0100

    chore: chore

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/state/ProcessExecutionCleanStateTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/state/ProcessExecutionCleanStateTest.java
@@ -45,7 +45,9 @@ public final class ProcessExecutionCleanStateTest {
           ZbColumnFamilies.PROCESS_CACHE_BY_ID_AND_VERSION,
           ZbColumnFamilies.PROCESS_CACHE_DIGEST_BY_ID,
           ZbColumnFamilies.MESSAGE_STATS,
-          ZbColumnFamilies.MIGRATIONS_STATE);
+          ZbColumnFamilies.MIGRATIONS_STATE,
+          ZbColumnFamilies.DEPLOYMENT_RAW,
+          ZbColumnFamilies.PROCESS_DEFINITION_KEY_BY_PROCESS_ID_AND_DEPLOYMENT_KEY);
 
   @Rule public EngineRule engineRule = EngineRule.singlePartition();
 
--- zeebe/engine/src/test/resources/state/appliers/golden/Form_CREATED_v1.golden
+++ zeebe/engine/src/test/resources/state/appliers/golden/Form_CREATED_v1.golden
@@ -25,5 +25,6 @@ public class FormCreatedApplier implements TypedEventApplier<FormIntent, FormRec
     formState.storeFormInFormColumnFamily(value);
     formState.storeFormInFormByIdAndVersionColumnFamily(value);
     formState.updateLatestVersion(value);
+    formState.storeFormKeyByFormIdAndVersionTag(value);
   }
 }
